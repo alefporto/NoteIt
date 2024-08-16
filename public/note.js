dfcf6271ceed5c23038ca34b4259fdb8d2d19cd3
Alef Porto <falejoaoalef@gmail.com>
2024-08-16 10:29:58 -0300

    Renomeando variáveis de manipulação do HTML

--- public/note.js
+++ public/note.js
@@ -1,29 +1,29 @@
 import { selecionarNote, emitirTextoAlterado } from './socket-front-note.js';
 
 // Seleciona os elementos HTML que serão manipulados
-const tagTituloHTML = document.getElementById('titulo-pagina');
-const tagTituloNote = document.getElementById('titulo-note');
-const tagTextoNote  = document.getElementById('texto-note');
+const tituloHTML = document.getElementById('titulo-pagina');
+const tituloNote = document.getElementById('titulo-note');
+const textoNote = document.getElementById('texto-note');
 
 // Pega o nome da anotação pelos query params
 const params = new URLSearchParams(window.location.search);
 const nomeNote = params.get("nome");
 
 // Dinamiza o nome da anotação no HTML
-tagTituloHTML.innerHTML = nomeNote || "Anotação sem título";
-tagTituloNote.innerHTML = nomeNote || "Anotação sem título";
+tituloHTML.innerHTML = nomeNote || "Anotação sem título";
+tituloNote.innerHTML = nomeNote || "Anotação sem título";
 
 // Atualiza a anotação com o seu respectivo texto ao abrir
 selecionarNote(nomeNote);
 
 // Envia um evento para o servidor informando que o texto dessa anotação foi alterado
-tagTextoNote.addEventListener('keyup', () => {
-    emitirTextoAlterado(nomeNote, tagTextoNote.value);
+textoNote.addEventListener('keyup', () => {
+    emitirTextoAlterado(nomeNote, textoNote.value);
 })
 
 // Atualiza o campo de texto com o texto passado via parâmetro
-function atualizaCampoTexto(texto){
-    tagTextoNote.value = texto
+function atualizaCampoTexto(texto) {
+    textoNote.value = texto
 }
 
 export { atualizaCampoTexto };
